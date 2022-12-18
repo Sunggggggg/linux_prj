f9194c9166d135f29a4c9bf6cc0f3e9f9e29edb8
sky_konkuk <110399785+Sunggggggg@users.noreply.github.com>
2022-12-18 23:10:04 +0900

    Update main.c
    12/18

--- main.c
+++ main.c
@@ -4,16 +4,20 @@
 #include <fcntl.h>
 #include <time.h> // need -lrt option. means to use the real-time library
 
-#define DEBUG_R
+//#define DEBUG_R
+#define DEBUG
+#define DEBUG_BTN
+#define DEBUG_TIME
 
-#define DEATH_MATCH  1  // do until someone is defeated
-#define SERVIVAL     0  // do just three rounds
-#define USER         0  // score index
-#define RASPI        1
-#define DRAW    2
-#define WIN     1
-#define LOSE    0
-
+#define DEATH_MATCH     1  // do until someone is defeated
+#define SERVIVAL        0  // do just three rounds
+#define USER        0  // score index
+#define RASPI       1
+#define LED_OFF         2
+#define LED_WIN         1
+#define LED_LOSE        0
+#define SEC2nSEC    1000000000
+#define SEC2uSEC    1000000
 
 ////////////////////// clock timer //////////////////////
 
@@ -23,25 +27,29 @@ Pitime NOW() {
     clock_gettime(CLOCK_REALTIME, &gettime_now);
     return gettime_now;
 }
-int isTimePassed_us(Pitime* ref, int time_us) {
+
+int timePassed_us(Pitime* ref) {
+    int time_passed_nsec;
+    int time_passed_sec;
+    int underflow;
+
     // 1s = 1,000,000us, 1us = 1,000ns
-    int time_sec  = time_us / 1000000;
-    int time_nsec = (time_us - time_sec) * 1000;
-    int passed_sec = ref->tv_sec + time_sec;
-    int passed_nsec = ref->tv_nsec + time_nsec;
-    if (passed_nsec > 1000000000) { // control overflow
-        passed_sec++;
-        passed_nsec -= 1000000000;
-    }
     clock_gettime(CLOCK_REALTIME, &gettime_now);
-    return (gettime_now.tv_sec <= passed_sec) ? 0 : (gettime_now.tv_nsec < passed_nsec) ? 0 : 1;
+    time_passed_nsec = gettime_now.tv_nsec - ref->tv_nsec;
+    underflow = time_passed_nsec < 0 ? 1 : 0;
+    if (underflow) time_passed_nsec += 1 * SEC2nSEC;
+    time_passed_sec = gettime_now.tv_sec - ref->tv_sec - underflow;
+#ifdef DEBUG_TIME
+    if (time_passed_sec < 0) printf("timePassed_us : !\n");
+#endif
+    return time_passed_sec * SEC2uSEC + time_passed_nsec / 1000;
 }
 
 int myRand() {
     Pitime rand_seed;
     rand_seed = NOW();
 #ifdef DEBUG_R
-    printf("main : rand val : %d", (rand_seed.tv_nsec & 1));
+    printf("main : rand val : %d\n", (rand_seed.tv_nsec & 1));
 #endif
     return rand_seed.tv_nsec & 1;
 }
@@ -73,32 +81,42 @@ int toggle_button_state = 0;
 // update toggle button signal w/ signal debouncing
 void buttonUpdate() {
     const  int      debounce_time_us = 40;
-           char     buff;
+    char     buff;
     static char     last_button_state = '0';
     static char     curr_button_state = '0';
-    static Pitime   last_pushed = {0};
+    static Pitime   last_pushed = { 0 };
 
     read(dev_gpio, &buff, 1); // read pin 6
 
     if (buff != last_button_state) // if the button signal detected(pressed or noise),
-        last_pushed = NOW();         
-    else if (!isTimePassed_us(&last_pushed, debounce_time_us)) // count the time a little
+        last_pushed = NOW();
+    if (timePassed_us(&last_pushed) > debounce_time_us) // count the time a little
         if (buff != curr_button_state) { // if the button signal is still changed
             curr_button_state = buff;
-            if (curr_button_state == '1')
+            if (curr_button_state == '1') {
+#ifdef DEBUG_TIME
+                printf("buttonUpdate : pressed\n");
+#endif
                 toggle_button_state = !toggle_button_state;
+            }
         }
     last_button_state = buff; // last_button_state will follow the signal(pressed or noise).
 }
 
 void writeLED(const int winflag) {
-    char buff = '0';
-    if (winflag) {
-        buff++;
-        write(dev_gpio, &buff, 1);  // blue LED
-    }
-    else 
-        write(dev_gpio, &buff, 1);  // red LED
+    static int prev_winflag = '0';
+    if (prev_winflag == winflag) return;
+
+    char buff;
+    if (winflag == LED_WIN)
+        buff = '1'; // blue LED
+    else if (winflag == LED_OFF)
+        buff = '2'; // turn all LED off
+    else // (winflag == LED_LOSE)
+        buff = '0'; // red LED
+    write(dev_gpio, &buff, 1);
+
+    prev_winflag = winflag;
 }
 
 
@@ -106,10 +124,11 @@ void writeLED(const int winflag) {
 
 void playBuzzer(char song) {
     static char prev_song = 'i';
-    if (prev_song != song) {
-        write(dev_bzzr, &song, 1);
-        prev_song = song;
-    }
+    if (prev_song == song) return;
+
+    write(dev_bzzr, &song, 1);
+
+    prev_song = song;
 }
 
 
@@ -120,15 +139,15 @@ void playBuzzer(char song) {
 #define D3 0x04
 #define D4 0x08
 
-const char seg_num[10] = {0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xd8, 0x80, 0x90};
+const char seg_num[10] = { 0xc0, 0xf9, 0xa4, 0xb0, 0x99, 0x92, 0x82, 0xd8, 0x80, 0x90 };
 const char seg_dot = 0x7f;
 int FND(int* score) {
     unsigned short data[3];
     static int n = 0;
 
-    data[0] = (seg_num[score[0]] << 4) | D1;
-    data[2] = (seg_dot           << 4) | D3;
-    data[1] = (seg_num[score[1]] << 4) | D4;
+    data[0] = (~seg_num[score[0]] << 4) | D1;
+    data[2] = (~seg_dot << 4) | D3;
+    data[1] = (~seg_num[score[1]] << 4) | D4;
 
     write(dev_fnd, &data[n], 2);
     n++;
@@ -137,83 +156,127 @@ int FND(int* score) {
 
 
 ////////////////////// Servo Motor //////////////////////
+#define CENTER  5
+#define LEFT    0
+#define RIGHT   1
 
+int Motor(int result) {
+    char dir = '0';
+    if (result == LEFT)         dir = '0'; // Turn Left
+    else if (result == RIGHT)   dir = '2'; // Turn Right
+    else if (result == CENTER)  dir = '1'; // Center
+
+    write(dev_motor, &dir, 1);
+}
 
 
 ////////////////////// main //////////////////////
 
 int main(void) {
+    Pitime time_ref;
 
     // open character devices
     int opn_err = openAllDev();
     if (opn_err & ERR_OPN_GPIO) goto CDevOpenFatal;
-    
 
     // wait for the start button pressed (behave as toggle)
-    do {buttonUpdate();} 
-    while (!toggle_button_state);
+    do { buttonUpdate(); } while (!toggle_button_state);
 
     // game started. wait 2sec...
-    Pitime time_ref = NOW();
     int game_mode = SERVIVAL;
-    while (!isTimePassed_us(&time_ref, 2000000)) buttonUpdate();
+    time_ref = NOW();
+    while (timePassed_us(&time_ref) < (2 * SEC2uSEC)) buttonUpdate();
     if (toggle_button_state == 0) {
         toggle_button_state = 1;
         game_mode = DEATH_MATCH;
     }
-
+#ifdef DEBUG
+    printf("main : game starts. curr game mode : %s\n", game_mode == SERVIVAL ? "SERVIVAL" : "DEATH_MATCH");
+#endif
 
     // initialize variables for loop
+    int passed_time_from_ref;
     int score[2] = { 3, 3 };
     int stage_count = (game_mode == SERVIVAL) ? 3 : 999999999;
     int stage_result = 1;
     int rpi_dir, usr_dir, usr_dir0, usr_dir1;
-    Motor(0); //TODO: motor
     time_ref = NOW();
+
+    Motor(CENTER);
+#ifdef DEBUG
+    int current = 0;
+#endif
 
     while (toggle_button_state) {
         // Face Detecting...
 
         FND(score);
         buttonUpdate();
+        passed_time_from_ref = timePassed_us(&time_ref);
 
-        if(!isTimePassed_us(&time_ref, 700000)){ // ~0.7s
+        //************* switch(passed_time) *************//
+        if (passed_time_from_ref < (0.7 * SEC2uSEC)) {
 
+
+#ifdef DEBUG
+            if (current != 1) { printf("Stage 1\n"); current = 1; }
+#endif
             playBuzzer('a'); //cham cham cham! (only once)
-                        
-            Motor(0); //TODO: motor
-
-            usr_dir0 = //FIXME: 이용자 얼굴각도 읽어오기
             rpi_dir = myRand(); //is current system clock count odd? or even?
+            Motor(CENTER);  //TODO: motor set to 0 (only once)
+            //FIXME: get user face direction0.
 
-        } else if (!isTimePassed_us(&time_ref, 1400000)) { // ~1.4s
 
-            Motor(rpi_dir); //TODO: motor
+            //////////////    ~0.7s    //////////////
+        }
+        else if (passed_time_from_ref < (1.4 * SEC2uSEC)) {
 
-            usr_dir1 = //FIXME: 이용자 얼굴각도 읽어오기
 
-        } else if (!isTimePassed_us(&time_ref, 3100000)) { // ~3.1s
-            
-            usr_dir = (usr_dir0 - usr_dir1); //FIXME: 이용자 결정 판별하기
-            stage_result = Compare(rpi_dir, usr_dir);
-                
-            if (stage_result == 1) playBuzzer('b');  // win (user side)
-            else playBuzzer('c'); //stage_result == 0   lose
+#ifdef DEBUG
+            if (current != 2) { printf("Stage 2 : rpi_dir = %d, usr_dir0 = \n", rpi_dir); current = 2; }
+#endif
+            Motor(rpi_dir); //TODO: motor set to dir_rpi (only once)
+            //FIXME: get user face direction1.
 
-        } else { // after 3.1s
-            
-            if (stage_result == 1) {  // win (user side)
-                score[RASPI]--;
-                writeLED(WIN);
+
+            //////////////    ~1.4s    //////////////
+        }
+        else if (passed_time_from_ref < (4.1 * SEC2uSEC)) {
+
+
+#ifdef DEBUG
+            if (current != 3) { printf("Stage 3 : usr_dir1 = \n"); current = 3; }
+#endif
+            //FIXME: is_result_computed
+            if (stage_result == 1) {  // win (user side) 
+                playBuzzer('b');
+                writeLED(LED_WIN);
             }
-            else {                    // lose
-                score[USER]--;
-                writeLED(LOSE);
+            else { //stage_result == 0   lose
+                playBuzzer('c');
+                writeLED(LED_LOSE);
             }
+            //FIXME: compute user's decision and update the result.
 
-            if (--stage_count && (score[RASPI] && score[USER])) {
+
+            //////////////    ~4.1s    //////////////
+        }
+        else {
+            ////////////// after 4.1s  //////////////
+
+
+#ifdef DEBUG
+            if (current != 4) { printf("Stage 4\n"); current = 4; }
+#endif  
+            writeLED(LED_OFF);
+            if (stage_result == 1) score[RASPI]--; // win (user side)
+            else                   score[USER]--; // lose
+
+            if ((--stage_count >= 0) && (score[RASPI] && score[USER])) {
+                // if the game isn't over
                 time_ref = NOW();
-            } else { // if game ends.
+            }
+            else { // if the game is over.
                 if (score[USER] >= score[RASPI])
                     playBuzzer('d'); // user won  this game
                 else
@@ -224,7 +287,8 @@ int main(void) {
         }
     }
     time_ref = NOW();
-    while (!isTimePassed_us(&time_ref, 2000000));
+    while (timePassed_us(&time_ref) < (2 * SEC2uSEC));
+    writeLED(LED_OFF);
 
 CDevOpenFatal:
     closeAllDev();
@@ -267,7 +331,7 @@ void closeAllDev() {
         &dev_gpio,
         &dev_fnd
     };
-    for (int i = 0; i < 4; i++) 
+    for (int i = 0; i < 4; i++)
         if (*cdevs[i] > 0)
             close(*cdevs[i]);
 }
